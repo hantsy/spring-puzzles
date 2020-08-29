bb04f6a5811c815ba26f7b3ae6fd10d875890297
hantsy <hantsy@gmail.com>
2020-08-29 19:46:37 +0800

    clean codes.

--- jdbc-ds-vanilla/src/test/java/com/example/demo/mybatis/mapper/PostMapper.java
+++ jdbc-ds-vanilla/src/test/java/com/example/demo/mybatis/mapper/PostMapper.java
@@ -1,13 +1,15 @@
 package com.example.demo.mybatis.mapper;
 
 import com.example.demo.Post;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface PostMapper {
 
+    // The mapping design is shit
 //    @Results({
 //            @Result(property = "body", column = "BODY")
 //    })
