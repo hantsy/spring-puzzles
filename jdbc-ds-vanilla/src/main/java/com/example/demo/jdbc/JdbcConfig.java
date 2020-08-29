cec7bac4aabe8302b3fc07b908b53b921f4533d7
hantsy <hantsy@gmail.com>
2020-08-29 17:20:39 +0800

    disable forceQuote in jdbcMappingContext to fix the postgres SQL issue

--- jdbc-ds-vanilla/src/main/java/com/example/demo/jdbc/JdbcConfig.java
+++ jdbc-ds-vanilla/src/main/java/com/example/demo/jdbc/JdbcConfig.java
@@ -2,14 +2,18 @@ package com.example.demo.jdbc;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.core.convert.JdbcCustomConversions;
+import org.springframework.data.jdbc.core.mapping.JdbcMappingContext;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.data.relational.core.mapping.NamingStrategy;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Optional;
 
 @Configuration
 @EnableJdbcRepositories
@@ -23,6 +27,14 @@ public class JdbcConfig extends AbstractJdbcConfiguration {
     @Bean
     NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Override
+    @Bean
+    public JdbcMappingContext jdbcMappingContext(Optional<NamingStrategy> namingStrategy, JdbcCustomConversions customConversions) {
+        var mappingContext= super.jdbcMappingContext(namingStrategy, customConversions);
+        mappingContext.setForceQuote(false);
+        return mappingContext;
     }
 
     @Bean
