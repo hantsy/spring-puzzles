d47e667be613a6531c8acfb4a24f92787d39a55e
Hantsy Bai <hantsy@gmail.com>
2021-05-06 10:48:52 +0800

    rename project.

--- multi-ds-jms-jta/src/main/java/com/example/demo/customers/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.customers;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository  extends JpaRepository<CustomerEntity, Long> {
-}
