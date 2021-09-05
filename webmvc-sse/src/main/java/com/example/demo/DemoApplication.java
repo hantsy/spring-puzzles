d421e046dbb68c5c0969804022975d9c40b8933e
hantsy <hantsy@gmail.com>
2021-09-05 20:52:54 +0800

    chore: clean codes

--- webmvc-sse/src/main/java/com/example/demo/DemoApplication.java
+++ webmvc-sse/src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,8 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -25,6 +29,7 @@ import static org.springframework.web.servlet.mvc.method.annotation.SseEmitter.e
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -58,6 +63,42 @@ class SseController {
         // till the client close it or some exceptions occur.
         sseEmitter.complete();
         return sseEmitter;
+    }
+
+    // all sse clients
+    Set<SseEmitter> emitters= new LinkedHashSet<>();
+
+    @SneakyThrows
+    @GetMapping(value = "broadcast", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter broadcastMessages() {
+        SseEmitter sseEmitter = new SseEmitter();
+        emitters.add(sseEmitter);
+
+        sseEmitter.onCompletion(() -> {
+            log.debug("completed");
+            emitters.remove(sseEmitter);
+        });
+        sseEmitter.onError(error -> log.error(error.getMessage()));
+        sseEmitter.onTimeout(() -> {
+            log.debug("timeout...");
+            emitters.remove(sseEmitter);
+        });
+        return sseEmitter;
+    }
+
+    @Scheduled(initialDelay = 1000L, fixedRate = 500L)
+    public void emitMessages(){
+        this.emitters.forEach(e -> {
+            Set<ResponseBodyEmitter.DataWithMediaType> eventData = event()
+                    .id(UUID.randomUUID().toString())
+                    .data(new TextMessage("message at " + LocalDateTime.now()), MediaType.APPLICATION_JSON)
+                    .build();
+            try {
+                e.send(eventData);
+            } catch (IOException ex) {
+                e.completeWithError(ex);
+            }
+        });
     }
 
     @GetMapping(value = "events2", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
