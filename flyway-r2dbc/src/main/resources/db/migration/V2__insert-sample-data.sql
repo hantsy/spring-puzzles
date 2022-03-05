6b8da5c15440795650590a0cc071737117407df0
hantsy <hantsy@gmail.com>
2022-03-05 20:39:09 +0800

    feat: flyway and liquibase example

---
+++ flyway-r2dbc/src/main/resources/db/migration/V2__insert-sample-data.sql
@@ -0,0 +1,1 @@
+INSERT INTO persons(id, name) VALUES (1, 'Jack'), (2, 'Rose');
---
+++ liquibase-r2dbc/src/main/resources/db/changelog/changes/create-person-table-changelog-1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE persons
+(
+    id      INT          NOT NULL PRIMARY KEY,
+    name    VARCHAR(100) NOT NULL,
+    address VARCHAR(255) NOT NULL
+);
