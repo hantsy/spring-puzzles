9cc2d719658f382c3ea81595a77312c693cc5efe
hantsy <hantsy@gmail.com>
2023-12-23 10:39:06 +0800

    chore: clean codes

--- hibernate-reactive-mutiny-oracle/src/test/java/com/example/demo/TodoRepositoryTest.java
+++ hibernate-reactive-mutiny-oracle/src/test/java/com/example/demo/TodoRepositoryTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -26,10 +28,19 @@ public class TodoRepositoryTest {
 
     // see: https://java.testcontainers.org/modules/databases/oraclexe/
     @Container
-    static OracleContainer oracle = new OracleContainer("gvenzl/oracle-xe:21-slim-faststart")
+    static OracleContainer oracleContainer = new OracleContainer("gvenzl/oracle-xe:21-slim-faststart")
         .withDatabaseName("blogdb")
         .withUsername("user")
         .withPassword("password");
+
+    @DynamicPropertySource
+    static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", () -> "r2dbc:oracle://"
+            + oracleContainer.getHost() + ":" + oracleContainer.getFirstMappedPort()
+            + "/" + oracleContainer.getDatabaseName());
+        registry.add("spring.r2dbc.username", () -> oracleContainer.getUsername());
+        registry.add("spring.r2dbc.password", () -> oracleContainer.getPassword());
+    }
 
     @Autowired
     TodoRepository todos;
