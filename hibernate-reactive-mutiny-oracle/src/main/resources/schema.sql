d54836e8d5409a81336303720d703dcbea551351
hantsy <hantsy@gmail.com>
2023-12-30 15:08:03 +0800

    chore: clean codes

--- hibernate-reactive-mutiny-oracle/src/main/resources/schema.sql
+++ hibernate-reactive-mutiny-oracle/src/main/resources/schema.sql
@@ -5,19 +5,20 @@ CREATE SEQUENCE IF NOT EXISTS todos_seq START WITH 1;
 CREATE TABLE IF NOT EXISTS todos
 (
     id    NUMBER(10)  DEFAULT todos_seq.nextval NOT NULL,
-    title VARCHAR2(200)
+    title VARCHAR2(200),
+    PRIMARY KEY(id)
 );
 
 -- ALTER TABLE todos DROP CONSTRAINT IF EXISTS todos_pk;
 -- see: https://gist.github.com/jarrodhroberson/067c8dd7dc40482cd984bb937f8c9f42
-BEGIN
-    EXECUTE IMMEDIATE 'ALTER TABLE todos DROP CONSTRAINT todos_pk';
-EXCEPTION
-  WHEN OTHERS THEN
-    IF SQLCODE != -2443 THEN
-      RAISE;
-    END IF;
-END;
-
-ALTER TABLE todos
-    ADD CONSTRAINT todos_pk PRIMARY KEY (id);
+-- BEGIN
+--     EXECUTE IMMEDIATE 'ALTER TABLE todos DROP CONSTRAINT todos_pk';
+-- EXCEPTION
+--   WHEN OTHERS THEN
+--     IF SQLCODE != -2443 THEN
+--       RAISE;
+--     END IF;
+-- END;
+--
+-- ALTER TABLE todos
+--     ADD CONSTRAINT todos_pk PRIMARY KEY (id);
