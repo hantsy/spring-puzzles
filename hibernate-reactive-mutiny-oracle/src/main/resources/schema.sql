f4c5bbab85b5f9d1a7adfa911e41aa3b07912d9c
hantsy <hantsy@gmail.com>
2023-12-30 14:22:47 +0800

    chore: clean codes

--- hibernate-reactive-mutiny-oracle/src/main/resources/schema.sql
+++ hibernate-reactive-mutiny-oracle/src/main/resources/schema.sql
@@ -1,12 +1,14 @@
-CREATE SEQUENCE todos_seq START WITH 1;
 -- 23c supports `if not exists`
+-- see: https://oracle-base.com/articles/23c/if-not-exists-ddl-clause-23c
+CREATE SEQUENCE IF NOT EXISTS todos_seq START WITH 1;
+
 CREATE TABLE IF NOT EXISTS todos
 (
     id    NUMBER(10)  DEFAULT todos_seq.nextval NOT NULL,
     title VARCHAR2(200)
 );
 
--- ALTER TABLE todos
---     DROP CONSTRAINT IF EXISTS todos_pk;
+ALTER TABLE todos
+    DROP CONSTRAINT IF EXISTS todos_pk;
 ALTER TABLE todos
     ADD CONSTRAINT todos_pk PRIMARY KEY (id);
