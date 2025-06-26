c3cd25a1573a4b7e6b471fcaba0a329915cd9fc0
hantsy <hantsy@gmail.com>
2025-06-26 23:37:44 +0800

    commit

--- programming-models-2x/webflux-ktco-fn/src/test/kotlin/com/example/demo/TestDemoApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.demo
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-    fromApplication<DemoApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
